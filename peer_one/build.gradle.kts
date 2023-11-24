542df7029c5c242f5760f00a5d5b193fd3e62539
Sebastian Bejarano <slbejaranom@gmail.com>
2023-11-23 23:23:21 -0500

    Added kafka dependency and docker-compose for service

--- peer_one/build.gradle.kts
+++ peer_one/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    //kafka
+    implementation("org.apache.kafka:kafka-clients:2.8.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
